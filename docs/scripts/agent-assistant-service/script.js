ec71a0491fbd7d973a26dd36215394362c45b02d
chinarn <67701253+chinarn@users.noreply.github.com>
2021-11-29 22:01:58 +0800

    Update script.js

--- docs/scripts/agent-assistant-service/script.js
+++ docs/scripts/agent-assistant-service/script.js
@@ -6,12 +6,14 @@
 
 const responseMap = {
     1: "Please wait while I connect you to a voice agent",
-    2: "I could provide you a one-time discount",
+    2: "I could provide you a one-time offer of free sports channels",
     3: "We're sorry to hear about your experience.",
     4: "We're glad that you enjoy our service!",
     5: "Goodbye. Please don't forget to like and subscribe to our channel.",
     6: "<img src=https://assets-global.website-files.com/5de87425133808841c6e3b3e/61891febd278607ea13b9a5d_TV%20marketing%20top%20panel%20desktop%20(1).png>As our customers deserve the best entertainment experience, Astro is refreshing its content and packages to offer more choice, value, flexibility, and better experience in a streaming world as part of the new Astro experience. The all-new Astro packs are designed for customers in the streaming and connected world, where they can stream all the shows they love on Ultra Box and soon, Ulti Box. Customers can now enjoy their favourite shows all on Astro, from global streaming services like Netflix, Disney+ Hotstar, HBO GO, iQIYI, TVBAnywhere+ as well as all Astro’s flagship shows and the best of live sports in one place, on the big screen. Our new packages offer unbeaten value, a greater variety of content and greater convenience, all made better with our Astro & Broadband for better connectivity.",
-    7: "test2"
+    7: "<img src=https://assets-global.website-files.com/5fc0b76331240365f64b0633/6183f5c80cd5cc0fc8567f42_cass%20faq%20tv%201.png><br>Link: https://support.astro.com.my/question/how-are-cancellation-fee-calculated",
+    8: "1. With numerous OTT entrants, consumers are suffering from subscription fatigue. In fact, they are looking for a one stop shop platform where OTT services are integrated in one place. <br>2. Astro offers flexibility and convenience with new packs where customers can get the convenience of accessing a variety of content via Ultra Box as the single remote-control solution. <br>3. In addition, there will be cost savings for Astro customers to purchase both Netflix and Astro together. Even better deals available if customers also subscribe to broadband for a better-connected experience.",
+    9: "•eGG Network<br>•Astro SuperSport<br>•Astro SuperSport 2<br>•FOX SPORTS<br>•FOX SPORTS 2<br>•Eurosport<br>•Astro SuperSport 3<br>•Astro Supersport4<br>•FOX SPORTS 3<br>•beIn Sports<br>•beIN Sports MAX<br>•Arena HD<br>•FOX SPORTS 2 HD<br>•FOX SPORTS 3 HD<br>•Astro Supersport 2 HD<br>•Astro SuperSport 3 HD<br>•Astro SuperSport 4 HD"
 }
 
 const keywordMap = {
@@ -26,8 +28,10 @@ const keywordMap = {
     'thank': 4,
     'helpful': 4,
     'bye': 5,
-    'picture': 6,
-    'test' : 7
+    'why is astro': 6,
+    'cancellation' : 7,
+    'vs direct' : 8,
+    'sports': 9
 }
 
 export default {
