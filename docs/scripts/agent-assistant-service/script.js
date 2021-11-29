55766f90992fbba4bbd830befda98a400526cdbe
chinarn <67701253+chinarn@users.noreply.github.com>
2021-11-29 19:28:54 +0800

    Update script.js

--- docs/scripts/agent-assistant-service/script.js
+++ docs/scripts/agent-assistant-service/script.js
@@ -10,7 +10,7 @@ const responseMap = {
     3: "We're sorry to hear about your experience.",
     4: "We're glad that you enjoy our service!",
     5: "Goodbye. Please don't forget to like and subscribe to our channel.",
-    6: "picture",
+    6: "<img src=https://assets-global.website-files.com/5de87425133808841c6e3b3e/61891febd278607ea13b9a5d_TV%20marketing%20top%20panel%20desktop%20(1).png>As our customers deserve the best entertainment experience, Astro is refreshing its content and packages to offer more choice, value, flexibility, and better experience in a streaming world as part of the new Astro experience. The all-new Astro packs are designed for customers in the streaming and connected world, where they can stream all the shows they love on Ultra Box and soon, Ulti Box. Customers can now enjoy their favourite shows all on Astro, from global streaming services like Netflix, Disney+ Hotstar, HBO GO, iQIYI, TVBAnywhere+ as well as all Astro’s flagship shows and the best of live sports in one place, on the big screen. Our new packages offer unbeaten value, a greater variety of content and greater convenience, all made better with our Astro & Broadband for better connectivity.",
     7: "test"
 }
 
